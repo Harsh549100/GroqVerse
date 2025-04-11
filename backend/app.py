b049f81d77e1097edb8b24dc49856fb45662b22f
Harsh549100 <hshrivastava23032007@gmail.com>
2025-04-11 15:33:04 +0530

    .

--- backend/app.py
+++ backend/app.py
@@ -255,4 +255,6 @@ def clear_conversation():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+ 
