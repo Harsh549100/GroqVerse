bf86390758de9db60ac99b17dbe2075a231228b6
Harsh549100 <hshrivastava23032007@gmail.com>
2025-04-21 21:52:02 +0530

    Update scripts.js

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -484,7 +484,7 @@ async function sendMessage() {
         showTypingIndicator();
         
         // Send to backend
-        const response = await fetch('http://localhost:5000/api/chat', {
+        const response = await fetch('http://localhost:8080/api/chat',{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
